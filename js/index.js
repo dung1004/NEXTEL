2346ea2129b450a3c1b5f857e349d515240b0b9e
dung <you@example.com>
2019-08-08 09:51:51 +0700

    edit theo yeu cau

--- js/index.js
+++ js/index.js
@@ -2,33 +2,40 @@
 // wowjs
  new WOW().init();
 
-// xu ly xem chi tiet product
-var nutShow = document.querySelectorAll('.products .box-img-text span.xemthem'),
-	nutClose = document.querySelectorAll('.products .chitiet > i'),
-	boxProduct = document.querySelectorAll('.products .chitiet');
-
-for (var i = 0; i < nutShow.length; i++) 
-{
-	nutShow[i].onclick = function() 
+function xulyxemthem(nutShow, nutClose, boxProduct) {
+	// xu ly xem chi tiet product
+	for (var i = 0; i < nutShow.length; i++) 
 	{
-		var dataShow = this.getAttribute('data-show');
-		var itemProduct = document.getElementById(dataShow);
-
-		for (var k = 0; k < boxProduct.length; k++) 
+		nutShow[i].onclick = function() 
 		{
-			boxProduct[k].style.display = 'none';
-		}
-		itemProduct.style.display = 'block';
+			var dataShow = this.getAttribute('data-show');
+			var itemProduct = document.getElementById(dataShow);
 
-		for (var ki = 0; ki < nutClose.length; ki++) 
-		{
-			nutClose[ki].onclick = function () 
+			for (var k = 0; k < boxProduct.length; k++) 
 			{
-				itemProduct.style.display = 'none';
+				boxProduct[k].style.display = 'none';
+			}
+			itemProduct.style.display = 'block';
+
+			for (var ki = 0; ki < nutClose.length; ki++) 
+			{
+				nutClose[ki].onclick = function () 
+				{
+					itemProduct.style.display = 'none';
+				}
 			}
 		}
 	}
 }
+var nutShow = document.querySelectorAll('.products .box-img-text span.xemthem'),
+	nutShow1 = document.querySelectorAll('.cacbox .box .box-text p span.xemthem'),
+	nutClose = document.querySelectorAll('.products .chitiet > i'),
+	nutClose1 = document.querySelectorAll('.cacbox .box .box-hidden > i'),
+	boxProduct = document.querySelectorAll('.products .chitiet'),
+	boxProduct1 = document.querySelectorAll('.cacbox .box .box-hidden');
+	
+xulyxemthem(nutShow, nutClose, boxProduct);
+xulyxemthem(nutShow1, nutClose1, boxProduct1);
 
 // xu ly slide 
 $('.owl-carousel').owlCarousel({
